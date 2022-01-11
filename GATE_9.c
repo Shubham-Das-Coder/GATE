d59e3e3b28fa8a16f0eaf104305d1b241641cf85
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-11 23:27:42 +0530

    Add files via upload

---
+++ GATE_9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    char str1[] = "gate";
+    char str2[] = "gate";
+    if (str1 == str2)
+        printf("\nequal");
+    else
+        printf("\nunequal");
+}
