dadd3a0d521e554dd7501a08b5082ce61f602fab
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-13 09:52:28 +0530

    Add files via upload

---
+++ GATE_39.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void printxy(int x, int y)
+{
+    int *ptr;
+    x = 0;
+    ptr = &x;
+    y = *ptr;
+    *ptr = 1;
+    printf("%d%d", x, y);
+}
+int main()
+{
+    printxy(1, 1);
+}
